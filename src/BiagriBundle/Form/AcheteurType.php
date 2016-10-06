f4daed838f5abeeed408a57bba92ceb73de0d7dd
VERO56 <veronique.lejeune.56@gmail.com>
2016-10-07 01:11:05 +0200

    réalisation page acheteur

--- src/BiagriBundle/Form/AcheteurType.php
+++ src/BiagriBundle/Form/AcheteurType.php
@@ -5,6 +5,7 @@ namespace BiagriBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class AcheteurType extends AbstractType
 {
@@ -15,14 +16,17 @@ class AcheteurType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('societe')
-            ->add('nom')
-            ->add('prenom')
-            ->add('adresse')
-            ->add('cp')
-            ->add('ville')
-            ->add('tel')
-            ->add('mail')
+           
+            ->add('societe', 'text', array('label' => 'Societé'))
+            ->add('nom', 'text', array('label' => 'Nom'))
+            ->add('prenom', 'text', array('label' => 'Prénom'))
+            ->add('adresse', 'text', array('label' => 'Adresse'))
+            ->add('cp', 'text', array('label' => 'Code Postal'))
+            ->add('ville', 'text', array('label' => 'Ville'))
+            ->add('tel', 'text', array('label' => 'Téléphone'))
+            ->add('mail', 'text', array(
+                'label' => 'email',
+            ))
         ;
     }
     
