93c79edecd083dc55d8d2470155be80b226268a2
FGalerne <florian.galerne@free.fr>
2016-10-07 03:46:59 +0200

    Create page offre

---
+++ src/BiagriBundle/Controller/OffreController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace BiagriBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+use BiagriBundle\Entity\Offre;
+use BiagriBundle\Form\OffreType;
+
+/**
+ * Offre controller.
+ *
+ */
+class OffreController extends Controller
+{
+    /**
+     * Lists all Offre entities.
+     *
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $offres = $em->getRepository('BiagriBundle:Offre')->findAll();
+
+        return $this->render('BiagriBundle:offre:index.html.twig', array(
+            'offres' => $offres,
+        ));
+    }
+
+    /**
+     * Creates a new Offre entity.
+     *
+     */
+    public function newAction(Request $request)
+    {
+        $offre = new Offre();
+        $form = $this->createForm('BiagriBundle\Form\OffreType', $offre);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($offre);
+            $em->flush();
+
+            return $this->redirectToRoute('offre_show', array('id' => $offre->getId()));
+        }
+
+        return $this->render('BiagriBundle:offre:new.html.twig', array(
+            'offre' => $offre,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Offre entity.
+     *
+     */
+    public function showAction(Offre $offre)
+    {
+        $deleteForm = $this->createDeleteForm($offre);
+
+        return $this->render('BiagriBundle:offre:show.html.twig', array(
+            'offre' => $offre,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Offre entity.
+     *
+     */
+    public function editAction(Request $request, Offre $offre)
+    {
+        $deleteForm = $this->createDeleteForm($offre);
+        $editForm = $this->createForm('BiagriBundle\Form\OffreType', $offre);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($offre);
+            $em->flush();
+
+            return $this->redirectToRoute('offre_edit', array('id' => $offre->getId()));
+        }
+
+        return $this->render('BiagriBundle:offre:edit.html.twig', array(
+            'offre' => $offre,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Offre entity.
+     *
+     */
+    public function deleteAction(Request $request, Offre $offre)
+    {
+        $form = $this->createDeleteForm($offre);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($offre);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('offre_index');
+    }
+
+    /**
+     * Creates a form to delete a Offre entity.
+     *
+     * @param Offre $offre The Offre entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Offre $offre)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('offre_delete', array('id' => $offre->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
